daa49e817b22d79431c158953c357dc7022fe3f7
Kamil <kamil.tomala@ath.pl>
2019-08-22 11:15:22 +0200

    Add useState and links to gifs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [gifs, setGifs] = useState([
+    'https://media.giphy.com/media/3ohhwoWSCtJzznXbuo/giphy.gif',
+    'https://media.giphy.com/media/l46CbZ7KWEhN1oci4/giphy.gif',
+    'https://media.giphy.com/media/3ohzgD1wRxpvpkDCSI/giphy.gif',
+    'https://media.giphy.com/media/xT1XGYy9NPhWRPp4pq/giphy.gif',
+  ]);
   return <h1>Test</h1>;
 }
 
